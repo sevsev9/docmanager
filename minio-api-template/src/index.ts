bcde98081c66a8ff7d444fb76a9831712b51a398
Severin Mairinger <sevm@pm.me>
2021-08-27 20:40:40 +0200

    reinstated basic minio api template

--- minio-api-template/src/index.ts
+++ minio-api-template/src/index.ts
@@ -1,16 +1,15 @@
 import express, {Application, Request, Response, NextFunction} from "express";
 import {BucketItem, Client} from "minio";
-import Minio from "minio";
 import Multer from "multer";
-import MulterMinIO from "multer-minio-storage";
+const MulterMinIO = require("multer-minio-storage-engine");
 import bodyParser from "body-parser";
 
-require('dotenv').config({ path: __dirname+"/.env"});
+require('dotenv').config({ path: __dirname+"/../.env"});
 
 const app:Application = express();
 app.use(bodyParser.json({limit: "4mb"}));
 
-const minioClient:Client = new Minio.Client({
+const minioClient:Client = new Client({
     endPoint: process.env.MINIO_ENDPOINT!,
     port: parseInt((process.env.MINIO_PORT) ? process.env.MINIO_PORT : "9000", 10),
     useSSL: (process.env.MINIO_SECUER === 'true'),
@@ -50,4 +49,17 @@ app.get('/list', (req: Request, res: Response) => {
         res.send(str);
         res.end();
     });
+});
+
+
+minioClient.bucketExists(process.env.MINIO_BUCKET!, function (error: any, exists: boolean) {
+    if (error) {
+        return console.log(error);
+    } else if(exists){
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    } else {
+        return console.log(`Bucket "${process.env.MINIO_BUCKET}" doesn't exist! Please change the configuration or create the bucket.`);
+    }
 });
