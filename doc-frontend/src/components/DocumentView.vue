da698b9737b9c9a589d058bc407a302ff213ea18
Severin Mairinger <sevm@pm.me>
2021-09-05 15:54:19 +0200

    added basic view layout

---
+++ doc-frontend/src/components/DocumentView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "DocumentView"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ doc-frontend/src/views/Authenticate.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="center-container">
+      <div class="login">
+
+      </div>
+      <div class="register"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Authenticate"
+}
+</script>
+
+<style scoped>
+
+</style>
