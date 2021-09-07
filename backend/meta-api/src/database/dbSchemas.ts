d2c0b8a354a29177f1f0910f4992e7b74185b20e
Severin Mairinger <sevm@pm.me>
2021-09-07 22:38:43 +0300

    altered and extended dbSchema structure

--- backend/meta-api/src/database/dbSchemas.ts
+++ backend/meta-api/src/database/dbSchemas.ts
@@ -3,22 +3,28 @@ import {model, Schema} from "mongoose";
 import {Document, User} from "./dbTypes";
 
 const dbDocument = new Schema<Document>({
-    name: {type: String, required: true},
+    name: {type: String, required: true, unique: true},
+    type: {type: Object, required: true},
+    etag: {type: String, required: true, unique: true},
     tags: {type: Array, required: true},
-    content: {type: String, required: true},
     category: {type: String, required: true},
     description: {type: String, required: true},
     upload_date: {type: Date, required: true},
-    thumbnail_etag: {type: String, required: true},
-    from: {type: Date, required: false}  //when the document was issued or received by the user
+    access: {type: Array, required: true},
+    // Optionals
+    thumbnail_etag: {type: String, required: false},
+    content: {type: String, required: false},
+    from: {type: Date, required: false}, //when the document was issued or received by the user
+    additional_info: {type: Object, required: false},
 });
 
 const dbUser = new Schema<User>({
-    username: {type: String,required: true},
+    username: {type: String,required: true, unique: true},
     password: {type: String, required: true}, //sha512 encoded
+    email: {type: String, required: true, unique: true},
     nickname: {type: String, required: true},
     registration_date: {type: Date, required: true},
-    permissions: {type: Number, required: false, default: 0}
+    permissions: {type: Number, required: false, default: 0} // 0 -> normal user | 1 -> admin user | following are reserved
 });
 
 export let UserModel = model<User>('User',dbUser);
--- backend/meta-api/src/database/dbTypes.ts
+++ backend/meta-api/src/database/dbTypes.ts
@@ -1,27 +1,66 @@
 export interface Document {
-    name: String,
-    eTag: String,
-    tags: Array<String>,
-    content: String,
-    category: String,
-    description: String,
-    upload_date: Date,
-    thumbnail_etag: String,
-    from: Date  //when the document was issued or received by the user
+  name: String,
+  type: Object,
+  eTag: String,
+  size: Number, // number of the filesize in kB
+  tags: Array<String>,
+  category: String,
+  description: String,
+  upload_date: Date,
+  access: Array<Object>,
+  //Optionals
+  thumbnail_etag?: String, //if empty -> default thumbnail will be used
+  content?: String,  //contains some sort of document contend, can be html formatted
+  from?: Date  //when the document was issued or received by the user
+  additional_info?: Object
 }
 
 export interface User {
-    username: String,
-    password: String, //sha512 encoded
-    nickname: String,
-    registration_date: Date,
-    permissions: Number
+  username: String,
+  password: String, //sha512 encoded
+  email: String,
+  nickname: String,
+  registration_date: Date,
+  permissions: Number //
 }
 
 export enum CATEGORIES {
-    RECEIPT, LETTER, LAW_DOCUMENT,OTHER
+  RECEIPT, LETTER, LAW_DOCUMENT, OTHER
 }
 
-export enum Permissions {
 
+/**
+ * Bitset permissions:
+ * Bit 0: READ,
+ * Bit 1: WRITE
+ * Bit 2: MODIFY
+ * Bit 3: DELETE
+ * Bit 4: CREATOR
+ */
+export enum Permissions {
+  READ, WRITE, MODIFY, DELETE, CREATOR
+}
+
+/**
+ * Calculates the integer from given permission(s). Operates as a bitset.
+ * @param list A String Array containing one or multiple permissions.
+ */
+export function getPermissions(list: Array<String>) {
+  //@Todo
+}
+
+const x = {
+  _id: "MongoDB ObjectID",
+  name: "descriptive document.extension",
+  type: "some file type, e.g.: { 'type': 'image', 'extension': 'png'}",
+  etag: "minio-etag",
+  content: "<p>Some content which is in the file. this is optional and can be formatted in <b>html</b>.</p>",
+  access: [
+    {user: "some user _id", role: "Integer: A Bitset of 4 Bits: bit 0 -> read, 1 -> write, 3 -> modify, 4 -> delete"}
+  ],
+  from: "Date: uploaded",
+  created: "Date: When the document was created",
+  category: "The document category: e.g. 'receipt' or 'postal document'",
+  additional_info: "Object: some additional info for the file",
+  thumbnail_etag: "eTag of the minio thumbnail file. if left empty a default thumbnail will be used."
 }
