4024e0efa340f4a283ba7cd9423b6959562ab022
Severin Mairinger <sevm@pm.me>
2021-09-09 12:55:29 +0200

    added user handling routes

--- doc-frontend/src/store/fnc/userHandling.js
+++ doc-frontend/src/store/fnc/userHandling.js
@@ -1,35 +1,49 @@
 import axios from "axios";
+import bcrypt from "bcrypt";
 
 module.exports = {
   register(user) {
     return new Promise((resolve, reject) => {
-      resolve(`${user + reject}`);
+      const salt = bcrypt.genSaltSync(10);
+      user.password = bcrypt.hashSync(user.password, salt);
+      axios.post(process.env.USER_REGISTER, user).then(resolve).catch(reject);
     })
   },
   login(context, auth) {
     return new Promise((resolve, reject) => {
+      const salt = bcrypt.genSaltSync(10);
+      const usr = {
+        password: bcrypt.hashSync(auth.password, salt)
+      };
+      if (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(auth.key)) { //test for email
+        usr.email = auth.key
+      } else {
+        usr.username = auth.key
+      }
       axios.post(process.env.USER_LOGIN, auth).then(res => {
         context.commit('login', res.data.user);
         resolve(res.data.user);
       }).catch(reject);
     })
   },
-  delete(auth) {
-    return axios.post(process.env.USER_DELETE, auth);
+  delete() {  //delete user profile
+    return axios.post(process.env.USER_DELETE);
   },
-  modify(auth, config) {
+  modify(auth, config) { //@TODO Modify user
     return new Promise((resolve, reject) => {
       resolve(`${auth + config + reject}`);
     })
   },
-  getStatistics(auth) {
+  getStatistics(auth) { //@TODO Get user statistics
     return new Promise((resolve, reject) => {
       resolve(`${auth + reject}`);
     })
   },
-  getProfile(auth) {
+  getProfile() {
     return new Promise((resolve, reject) => {
-      resolve(`${auth + reject}`);
+      axios.get(process.env.USER_GET_PROFILE).then(res => {
+        resolve(res.data.user);
+      }).catch(reject)
     })
   }
 }
