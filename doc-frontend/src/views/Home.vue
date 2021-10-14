d77d5b909fe16270453222db17a50e12891ad33d
Severin Mairinger <sevm@pm.me>
2021-10-14 17:58:23 +0200

    added todo list to home

--- doc-frontend/src/views/Home.vue
+++ doc-frontend/src/views/Home.vue
@@ -1,6 +1,19 @@
 <template>
-  <div><h1>docmanager - Document Management Software</h1>
+  <div>
+    <h1>docmanager - Document Management Software</h1>
     <p>Your secure and <a href="https://github.com/sevsev9/docmanager">open source</a> document management software.</p>
+
+    <ul>
+      <li>@Todo: Add OAuth Registration & Login</li>
+      <li>@Todo: Add Dashboard: <br>
+        <ol>
+          <li>File Upload Screen</li>
+          <li>File Download/Export Screen</li>
+          <li>Landing page and statistics</li>
+          <li>Create export links (sharing links)</li>
+        </ol>
+      </li>
+    </ul>
   </div>
 </template>
 
