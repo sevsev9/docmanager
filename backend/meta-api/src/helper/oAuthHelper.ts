470a9ac85c4e11ab1f94d8b36493906f66a924e4
Severin Mairinger <sevm@pm.me>
2021-10-20 11:56:41 +0200

    added oauth get email backend

--- backend/meta-api/src/helper/oAuthHelper.ts
+++ backend/meta-api/src/helper/oAuthHelper.ts
@@ -3,10 +3,9 @@ import axios from "axios";
 /**
  * Checks if the access_token is valid and the email address for it matches the given.
  * @param access_token The Google OAuth Access Token
- * @param email The email to be compared with.
- * @returns boolean True if the authentication information is valid.
+ * @returns boolean The users email if the access token is valid. Otherwise rejects.
  */
-export function testAccessToken(access_token: string, email: string) {
+export function getEmailFromAccessToken(access_token: string) {
   return new Promise<any>( (resolve, reject) => {
     axios.get("https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=" + access_token).then(res => {
       console.log(res); //@Todo
