820e8184605c1d67ac39f8ad8c34adc6bce4d07d
Severin Mairinger <sevm@pm.me>
2021-10-24 16:06:40 +0200

    started work on file upload

--- doc-frontend/src/views/file/FileUpload.vue
+++ doc-frontend/src/views/file/FileUpload.vue
@@ -1,15 +1,41 @@
 <template>
   <div>
-    <h1>Uploads</h1>
+    <h1>Upload a File</h1>
+    <div class="document-upload">
+      <b-form-file
+          v-model="file1"
+          :state="Boolean(file1)"
+          placeholder="Choose a file or drop it here..."
+          drop-placeholder="Drop file here..."
+          required
+      ></b-form-file>
+      <b-form-group
+          id="document-upload-metadata"
+          label="Please add the metadata for the document as desired."
+      >
+
+      </b-form-group>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "FileUpload"
+  name: "FileUpload",
+  data: () => {
+    return {
+      file1: undefined
+    }
+  },
+  methods: {}
 }
 </script>
 
 <style scoped>
+
+.document-upload {
+  width: 30%;
+  margin: auto;
+}
 
 </style>
